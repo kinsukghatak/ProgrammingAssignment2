e802c6fb7a0fae779715762f6153647d08481485
kinsukghatak <kinsukghatak@gmail.com>
2015-06-22 00:36:07 +0530

    Programming done
    Changed and re written the program

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+##makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+makeCacheMatrix <- function(x= matrix ()) {
+m<- null
 
-## Write a short comment describing this function
+##set the matrix
+set<- function(y) {
+x<<- y
+m<<- null }
 
-makeCacheMatrix <- function(x = matrix()) {
+##get the matrix
+get <- function() { x }
 
+##set inverse of the matrix
+setinverse <- function(inverse) { m <<- inverse }
+
+##get inverse of the matrix
+getinverse <- function()  { m }
+
+## list of all argument
+list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+##cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+##If the inverse has already been calculated, then the cachesolve should retrieve the inverse from the cache.
+cachesolve <- function(x, ...) {
+ 
+##return of the inverse matrix if it already exists
+m <- x$getinverse()
+if(!is.null(m)) {
+message("getting cached data")
+return(m)
+}
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+##get the matrix from dataset
+data <- x$get()
+
+##  perform inverse operation for non-singluar sq matrix
+m <- solve(data, ...)
+
+##set the inverse of the object
+x$setinverse(m)
+       
+## return the matrix
+m
 }
